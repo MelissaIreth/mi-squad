fda81480c9c8b64428a044ebc56a374d8379768e
Melissa Pacheco <melissa.pacheco.martinez@gmail.com>
2017-05-04 18:16:19 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function MiembroSquad(nombre,apellido,edad,hobbies){
+	this.nombre = nombre
+	this.apellido = apellido
+	this.edad = edad
+	this.hobbies = hobbies
+}
+
+const Tania = new MiembroSquad("Tania","Ramirez","veintisiempre","Viajes, Gatos, Tomar técito a las 4")
