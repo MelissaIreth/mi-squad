12477920ad8bddb037c42973a6be5721e4284728
Melissa Pacheco <melissa.pacheco.martinez@gmail.com>
2017-05-04 22:30:04 -0300

    correciones

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,15 +7,15 @@ function MiembroSquad(nombre,apellido,edad,hobbies){
 
 var Tania = new MiembroSquad("Tania","Ramirez","veintisiempre","Viajes, Gatos, Tomar técito a las 4")
 var Pauli = new MiembroSquad("Paulina", "Aros" , 28,"Hacer libretas, Acariciar gatos, Manicure")
-var Karla = new MiembroSquad("Karla", "Jeria", 35,"comer chocolates, viajar,ir al cine")
-var Amala = new MiembroSquad("Valentina Amala"," Kamala", 29, "bordar, tejer, dormir")
-var Paula = new MiembroSquad("Paula", "Ponce", 28, "música, aves, heroes of the storm")
-var Mariela = new MiembroSquad("Mariela", "Cubillos", 36, "tejer, Comer chocolate y cocinar")
-var Marcela = new MiembroSquad("Marcela", "Cabello",31,"cantar, yoga, cocinar")
-
+var Karla = new MiembroSquad("Karla", "Jeria", 35,"Comer chocolates, Viajar, Ir al cine")
+var Amala = new MiembroSquad("Valentina Amala"," Kamala", 29, "Bordar, Tejer, Dormir")
+var Paula = new MiembroSquad("Paula", "Ponce", 28, "Música, Aves, Heroes of the storm")
+var Mariela = new MiembroSquad("Mariela", "Cubillos", 36, "Tejer, Comer chocolate, Cocinar")
+var Marcela = new MiembroSquad("Marcela", "Cabello",31,"Cantar, Yoga, Cocinar")
+var Meli = new MiembroSquad("Melissa", "Pacheco", 25, "Dormir, Comer, Matilda")
 var arr=[]
-arr.push(Tania,Pauli,Karla,Amala,Paula,Mariela,Marcela)
+arr.push(Tania,Pauli,Karla,Amala,Paula,Mariela,Marcela,Meli)
 
 arr.forEach(function(cv){
-   document.write("<div>"+"<b>Nombre: </b>"+cv.nombre+" "+cv.apellido+"<ul><li><b>Edad: </b>" +cv.edad+"<li><b>Hobbies: </b><i>"+cv.hobbies+"</li></ul></i><br></br></div>")
+   document.write("<div>"+"<b>Nombre: "+cv.nombre+" "+cv.apellido+"<ul><li>Edad: " +cv.edad+"<li>Hobbies: <i>"+cv.hobbies+"</li></ul></i><br></b></div>")
 }) 
